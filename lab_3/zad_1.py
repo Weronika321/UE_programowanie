7e3f6a858b2673de3c4082536ecbbcf0f08d1637
Weronika Ładak <91367748+Weronika321@users.noreply.github.com>
2021-11-07 17:59:06 +0100

    Delete lab_3 directory

--- lab_3/zad_1.py
+++
@@ -1,7 +0,0 @@
-def greeting(name: str, surname: str) -> str:
-    return f"Cześć {name} {surname}!"
-
-
-hello = greeting("Ania", "Kowalska")
-
-print(hello)
--- lab_3/zad_2.py
+++
@@ -1,5 +0,0 @@
-def multiplication(number_1: int, number_2: int) -> int:
-    return number_1 * number_2
-
-
-print(multiplication(2, 3))
