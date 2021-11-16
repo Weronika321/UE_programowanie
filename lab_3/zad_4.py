6aabe7a3e59a07733a1f7619dc9eb9f71b1d6dca
Weronika Ładak <91367748+Weronika321@users.noreply.github.com>
2021-11-16 14:20:12 +0100

    Delete lab_3 directory

--- lab_3/zad_4.py
+++
@@ -1,6 +0,0 @@
-def test(number_1: int, number_2: int, number_3: int) -> bool:
-    return number_1 + number_2 >= number_3
-
-
-print(test(1, 2, 1))
-print(test(2, 2, 5))
--- lab_3/zad_6.py
+++
@@ -1,6 +0,0 @@
-def power(list_1: list, list_2: list) -> list:
-    list_1 += list_2
-    return [element ** 3 for element in list(set(list_1))]
-
-
-print(power([1, 2, 4, 8, 11], [1, 3, 7, 9, 12, 11, 8]))
