aab560c48786d9910006afc1127858ffa527b7fe
Weronika Ładak <weronika.ladak1@gmail.com>
2021-11-23 21:04:00 +0100

    Nowe dane

--- lab_7/functions/file_to_list.py
+++ lab_7/functions/file_to_list.py
@@ -1,11 +1,12 @@
+import csv
 from io import TextIOWrapper
 from classes.Movie import Movie
 
 
 def file_to_list(file_reader: TextIOWrapper) -> list:
     result = []
-    for row in file_reader.readlines():
-        row_list = list(row.split(","))
-        result.append(Movie(row_list[0], row_list[1], row_list[2]))
+
+    for row in csv.reader(file_reader, delimiter=","):
+        result.append(Movie(row[0], row[1], row[2]))
 
     return result
