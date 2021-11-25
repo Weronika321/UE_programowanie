04d880394da1dfca20562a59d8bf7fd7b1e8fd4b
Weronika Ładak <weronika.ladak1@gmail.com>
2021-11-25 06:47:47 +0100

    Poprawki z pycodestyle

--- lab_7/classes/Ratings.py
+++ lab_7/classes/Ratings.py
@@ -1,26 +1,28 @@
-class Ratings():
-    def __init__(self, userId: int, movieId: int, rating: float, timestamp: int) -> None:
+class Ratings:
+    def __init__(
+        self, userId: int, movieId: int, rating: float, timestamp: int
+    ) -> None:
         self._userId = userId
         self._movieId = movieId
         self._rating = rating
         self._timestamp = timestamp
-    
+
     @property
     def userId(self):
         return self._userId
-    
+
     @property
     def movieId(self):
         return self._movieId
-    
+
     @property
     def rating(self):
         return self._rating
-    
+
     @property
     def timestamp(self):
         return self._timestamp
-    
+
     def __dict__(self):
         return {
             "userId": f"{self._userId}",
