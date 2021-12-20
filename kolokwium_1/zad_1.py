c90268dc10b9f05558e0231a7db57712b3232100
Weronika Ładak <weronika.ladak1@gmail.com>
2021-12-20 20:32:40 +0100

    nauka do kolokwium_2

--- kolokwium_1/zad_1.py
+++ kolokwium_1/zad_1.py
@@ -1,6 +1,12 @@
-class Kurs():
-    def __init__(self, lista_odcinkow: list, kierowca: str,
-                 auto: str, czas: str, odleglosc_km: list) -> None:
+class Kurs:
+    def __init__(
+        self,
+        lista_odcinkow: list,
+        kierowca: str,
+        auto: str,
+        czas: str,
+        odleglosc_km: list,
+    ) -> None:
         self._lista_odcinkow = lista_odcinkow
         self._kierowca = kierowca
         self._auto = auto
@@ -8,19 +14,19 @@ class Kurs():
         self._odleglosc_km = odleglosc_km
 
     @property
-    def lista_odcinkow(self):
+    def lista_odcinkow(self) -> list:
         return self._lista_odcinkow
 
     @property
-    def kierowca(self):
+    def kierowca(self) -> str:
         return self._kierowca
 
     @property
-    def auto(self):
+    def auto(self) -> str:
         return self._auto
 
     @property
-    def czas(self):
+    def czas(self) -> str:
         return self._czas
 
     @property
@@ -61,9 +67,15 @@ class Kurs():
         return self._auto
 
 
-class Pojazd():
-    def __init__(self, liczba_kol: int, marka: str, rodzaj: str,
-                 wlasciciel: str, liczba_miejsc: int) -> None:
+class Pojazd:
+    def __init__(
+        self,
+        liczba_kol: int,
+        marka: str,
+        rodzaj: str,
+        wlasciciel: str,
+        liczba_miejsc: int,
+    ) -> None:
         self._liczba_kol = liczba_kol
         self._marka = marka
         self._rodzaj = rodzaj
@@ -71,23 +83,23 @@ class Pojazd():
         self._liczba_miejsc = liczba_miejsc
 
     @property
-    def liczba_kol(self):
+    def liczba_kol(self) -> int:
         return self._liczba_kol
 
     @property
-    def marka(self):
+    def marka(self) -> str:
         return self._marka
 
     @property
-    def rodzaj(self):
+    def rodzaj(self) -> str:
         return self._rodzaj
 
     @property
-    def wlasciciel(self):
+    def wlasciciel(self) -> str:
         return self._wlasciciel
 
     @property
-    def liczba_miejsc(self):
+    def liczba_miejsc(self) -> int:
         return self._liczba_miejsc
 
     def __str__(self) -> str:
@@ -100,10 +112,15 @@ class Pojazd():
                 """
 
 
-class FirmaTransportowa():
-    def __init__(self, wlasciciel_firmy: str, powierzchnia_firmy: float,
-                 miejscowosc_firmy: str, ulica_firmy: str,
-                 kod_pocztowy_firmy: str) -> None:
+class FirmaTransportowa:
+    def __init__(
+        self,
+        wlasciciel_firmy: str,
+        powierzchnia_firmy: float,
+        miejscowosc_firmy: str,
+        ulica_firmy: str,
+        kod_pocztowy_firmy: str,
+    ) -> None:
         self._wlasciciel_firmy = wlasciciel_firmy
         self._powierzchnia_firmy = powierzchnia_firmy
         self._miejscowosc_firmy = miejscowosc_firmy
@@ -111,23 +128,23 @@ class FirmaTransportowa():
         self._kod_pocztowy_firmy = kod_pocztowy_firmy
 
     @property
-    def wlasciciel_firmy(self):
+    def wlasciciel_firmy(self) -> str:
         return self._wlasciciel_firmy
 
     @property
-    def powierzchnia_firmy(self):
+    def powierzchnia_firmy(self) -> float:
         return self._powierzchnia_firmy
 
     @property
-    def miejscowosc_firmy(self):
+    def miejscowosc_firmy(self) -> str:
         return self._miejscowosc_firmy
 
     @property
-    def ulica_firmy(self):
+    def ulica_firmy(self) -> str:
         return self._ulica_firmy
 
     @property
-    def kod_pocztowy_firmy(self):
+    def kod_pocztowy_firmy(self) -> str:
         return self._kod_pocztowy_firmy
 
     def __str__(self) -> str:
@@ -138,68 +155,81 @@ class FirmaTransportowa():
                 """
 
 
-class Odcinek():
-    def __init__(self, start: str, meta: str, odleglosc_km: float, czas_h: float, kierowca: str) -> None:
+class Odcinek:
+    def __init__(
+        self,
+        start: str,
+        meta: str,
+        odleglosc_km: float,
+        czas_h: float,
+        kierowca: str,
+    ) -> None:
         self._start = start
         self._meta = meta
         self._odleglosc_km = odleglosc_km
         self._czas_h = czas_h
         self._kierowca = kierowca
-        
+
     @property
-    def start(self):
+    def start(self) -> str:
         return self._start
-    
+
     @property
-    def meta(self):
+    def meta(self) -> str:
         return self._meta
-    
+
     @property
-    def odleglosc_km(self):
+    def odleglosc_km(self) -> float:
         return self._odleglosc_km
-    
+
     @property
-    def czas_h(self):
+    def czas_h(self) -> float:
         return self._czas_h
-    
+
     @property
-    def kierowca(self):
+    def kierowca(self) -> str:
         return self._kierowca
-    
+
     def __str__(self) -> str:
         return f"""Odcinek od {self._start} do {self._meta}. \n
                 Odległość {self._odleglosc_km}km przebyta w czasie {self._czas_h}h przez {self._kierowca}"""
-        
 
-class FirmaSpozywcza():
-    def __init__(self, nazwa_firmy: str, wlasciciel_firmy: str, 
-                 powierzchnia_firmy: float, liczba_pracownikow: int, adres_firmy: str) -> None:
+
+class FirmaSpozywcza:
+    def __init__(
+        self,
+        nazwa_firmy: str,
+        wlasciciel_firmy: str,
+        powierzchnia_firmy: float,
+        liczba_pracownikow: int,
+        adres_firmy: str,
+    ) -> None:
         self._nazwa_firmy = nazwa_firmy
         self._wlasciciel_firmy = wlasciciel_firmy
         self._powierzchnia_firmy = powierzchnia_firmy
         self._liczba_pracownikow = liczba_pracownikow
         self._adres_firmy = adres_firmy
-        
+
     @property
-    def nazwa_firmy(self):
+    def nazwa_firmy(self) -> str:
         return self._nazwa_firmy
-    
+
     @property
-    def wlasciciel_firmy(self):
+    def wlasciciel_firmy(self) -> str:
         return self._wlasciciel_firmy
-    
+
     @property
-    def powierzchnia_firmy(self):
+    def powierzchnia_firmy(self) -> float:
         return self._powierzchnia_firmy
-    
+
     @property
-    def liczba_pracownikow(self):
+    def liczba_pracownikow(self) -> int:
         return self._liczba_pracownik
-    
+
     @property
-    def adres_firmy(self):
+    def adres_firmy(self) -> str:
         return self._adres_firmy
-    
+
     def __str__(self) -> str:
         return f"""Firma spożywcza:\n\t
                     nazwa firmy: {self.nazwa_firmy}\n\t
@@ -207,17 +237,29 @@ class FirmaSpozywcza():
                     powierzchnia firmy: {self._powierzchnia_firmy}\n\t
                     liczba pracownikow: {self._liczba_pracownikow}\n\t
                     adres firmy: {self._adres_firmy}"""
-                    
-firma_tr = FirmaTransportowa("Adam Nowak", 102.3, "Katowice", "Wyszyńskiego 6", "21-345")
-firma_sp_1 = FirmaSpozywcza("ABC", "Kamil Baran", 121.5, 6, "Chorzów, ul. Kościuszki 64")
-firma_sp_2 = FirmaSpozywcza("Sklepik osiedlowy", "Ewa Nowakowska", 76, 3, "Katowice, ul. Zielona 75")
+
+
+firma_tr = FirmaTransportowa(
+    "Adam Nowak", 102.3, "Katowice", "Wyszyńskiego 6", "21-345"
+)
+firma_sp_1 = FirmaSpozywcza(
+    "ABC", "Kamil Baran", 121.5, 6, "Chorzów, ul. Kościuszki 64"
+)
+firma_sp_2 = FirmaSpozywcza(
+    "Sklepik osiedlowy", "Ewa Nowakowska", 76, 3, "Katowice, ul. Zielona 75"
+)
 
 pojazd1 = Pojazd(4, "BMW", "dostawczy", "Adrian Korek", 3)
 
 odcinek1 = Odcinek(firma_tr, firma_sp_1, 10.5, 0.5, "Jan Kowalski")
 odcinek2 = Odcinek(firma_sp_1, firma_sp_2, 7.25, 1.5, "Jan Kowalski")
 odcinek3 = Odcinek(firma_sp_2, firma_tr, 6.475, 0.5, "Jan Kowalski")
-                 
-kurs1 = Kurs([firma_tr, firma_sp_1, firma_sp_2, firma_tr], "Jan Kowalski", 
-             pojazd1, "2.5", [odcinek1.odleglosc_km, odcinek2.odleglosc_km, odcinek3.odleglosc_km])
+
+kurs1 = Kurs(
+    [firma_tr, firma_sp_1, firma_sp_2, firma_tr],
+    "Jan Kowalski",
+    pojazd1,
+    "2.5",
+    [odcinek1.odleglosc_km, odcinek2.odleglosc_km, odcinek3.odleglosc_km],
+)
 print(kurs1)
