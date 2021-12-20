5d419f7c88e9de9a788632271d6bd616aabe17dd
Weronika Ładak <weronika.ladak1@gmail.com>
2021-12-20 20:11:13 +0100

    notatki z zajęć

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ pytesseract.pytesseract.tesseract_cmd = (
 def read_text(filename: str) -> str:
     img_cv = cv2.imread(filename)
     img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+
     return pytesseract.image_to_string(img_rgb)
 
 
@@ -22,3 +23,16 @@ paths = [
 ]
 
 [print(read_text(path)) for path in paths]
+
+
+""" cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2) """
